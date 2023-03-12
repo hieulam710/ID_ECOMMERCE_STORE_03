615b24d36b88516c3c658d0164655656881f8c12
hieulam710 <trunghieule.htc@gmail.com>
2023-03-12 20:47:21 +0700

    push lai

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -4,12 +4,13 @@ import App from './App';
 import CategoriesPage from './Pages/categoriesPage/CategoriesPage';
 import LoginPage from './Pages/login-register/LoginPage';
 import RegisterPage from './Pages/login-register/RegisterPage';
+import ProductDetail from './Pages/productDetail/ProductDetail';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CategoriesPage/>
+    <ProductDetail/>
   </React.StrictMode>
 );
 
